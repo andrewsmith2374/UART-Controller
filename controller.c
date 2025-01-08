8aea60658b81baae60a5418a3c019119c716de57
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-01-08 10:41:42 -0500

    Update controller.c to use header file

--- controller.c
+++ controller.c
@@ -1,23 +1,19 @@
 #include <stdlib.h>
 #include <pthread.h>
-#include "libuart-0.2.0.1/uart.h"
+#include "controller.h"
 
 #define DEV_NAME "S0"
 #define BAUD_RATE 9600
 #define OPT "8N1N"
 
-void *fault_handler();
-
-void *control_loop();
-
 int main(void) {
-    pthread_t *fault_thread, *control_thread;
+    pthread_t fault_thread, control_thread;
     
     uart_t *remote_sensor = uart_open(DEV_NAME, BAUD_RATE, OPT);
     if (remote_sensor == NULL) { return -1; }
 
-    pthread_create(fault_thread, NULL, &fault_handler, NULL);
-    pthread_create(control_thread, NULL, &control_loop, NULL);
+    pthread_create(&fault_thread, NULL, &fault_handler, NULL);
+    pthread_create(&control_thread, NULL, &control_loop, NULL);
 
     // Main Loop
 }
