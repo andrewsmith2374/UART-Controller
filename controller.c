57b5b88df2c9a94686fa25034b4b66022dc3b41d
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-01-07 19:38:32 -0500

    Open UART port

--- controller.c
+++ controller.c
@@ -1,2 +1,12 @@
+#include <stdlib.h>
 #include "libuart-0.2.0.1/uart.h"
 
+#define DEV_NAME "S0"
+#define BAUD_RATE 9600
+#define OPT "8N1N"
+
+int main(void) {
+    
+    uart_t *remote_sensor = uart_open(DEV_NAME, BAUD_RATE, OPT);
+    if (remote_sensor == NULL) { return -1; }
+}
