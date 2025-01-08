e7cbfd760f8aa026d5917fdbd4fb59e140acf0ce
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-01-08 14:34:10 -0500

    Update headers

--- controller.h
+++ controller.h
@@ -1,5 +1,8 @@
 #include "libuart-0.2.0.1/uart.h"
 #include "libuart-0.2.0.1/error.h"
+#include "utils.c"
+
+#define DATA_REC "1"
 
 void *fault_handler();
-void *control_loop();
+void *control_loop(uart_t *sensor);
