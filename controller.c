d5e191211fc790dad4703319f5b4b0ccd5c9b27f
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-01-09 13:05:28 -0500

    Add test loop

--- controller.c
+++ controller.c
@@ -46,6 +46,7 @@ int main(void) {
         if (p > 0) { // Parent again
             close(control_fds[0]);
             main_loop(remote_sensor, fault_fds[1], control_fds[1]);
+            test_loop(fault_fds[1], control_fds[1]);
         } else if (p == 0) { // Child
             close(fault_fds[1]);
             close(control_fds[1]);
@@ -83,6 +84,25 @@ void main_loop(uart_t *remote_sensor, int fault_fd, int control_fd) {
             writefd(fault_fd, DATA_REC, sizeof(char *));
             writefd(control_fd, buf, sizeof(char *));
         }
+        // Sleep for 100ms to reduce overhead while maintaining timing precision
+        nanosleep(&sleep_interval, NULL);
+    }
+}
+
+void test_loop(int fault_fd, int control_fd) {
+
+    char *buf = "25";
+    long temp;
+
+    struct timespec sleep_interval;
+    sleep_interval.tv_sec = 1;
+    sleep_interval.tv_nsec = 0;
+
+    while (1) { // Busy waiting but library doesn't provide anything better
+
+        writefd(fault_fd, DATA_REC, sizeof(char *));
+        writefd(control_fd, buf, sizeof(char *));
+
         // Sleep for 100ms to reduce overhead while maintaining timing precision
         nanosleep(&sleep_interval, NULL);
     }
