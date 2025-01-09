f1e22ebc22143619cead40f754c1429f022123f8
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-01-09 10:44:56 -0500

    Add control flow for fault_handler

--- controller.h
+++ controller.h
@@ -11,3 +11,5 @@ void *fault_handler(int fd);
 void *control_loop(int fd, uart_t *sensor);
 
 void main_loop(uart_t *remote_sensor, int fault_fd, int control_fd);
+void handle_fault();
+void clear_fault();
--- fault_handler.c
+++ fault_handler.c
@@ -1,4 +1,30 @@
 #include <stdlib.h>
 #include "controller.h"
 
-void *fault_handler(int fd) { return NULL; }
+void *fault_handler(int fd) {
+
+    struct timeval timeout;
+    int ret;
+    fd_set set;
+
+    // Initialize set
+    FD_ZERO(&set);
+    FD_SET(fd, &set);
+
+    // Set timeout values
+    timeout.tv_sec = TIMEOUT;
+    timeout.tv_usec = 0; /* microseconds */
+
+    while (1) { // Read from pipe
+
+        ret = select(fd + 1, &set, NULL, NULL, &timeout);
+        if (ret == 0) { // Timeout occurred
+            handle_fault();
+        } else if (ret < 0) {
+            perror("signal");
+            exit(-1);
+        } else {
+            clear_fault();
+        }
+    }
+}
